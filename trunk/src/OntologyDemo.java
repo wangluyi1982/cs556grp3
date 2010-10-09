c0c7010306d4422f79490f1819c7a5b35a88ae86
w.w.mensah@gmail.com <w.w.mensah@gmail.com@f61b3770-9b39-c3b9-6f90-abcdacec213d>
2010-10-09 05:44:34 +0000

    java code to create ontology tree in sqlite database

---
+++ trunk/src/OntologyDemo.java
@@ -0,0 +1,19 @@
+package org.coode.owlapi.examples;
+
+public class OntologyDemo {
+	
+	public static void main(String[] args) throws Exception{
+		OntologyTree ont = new OntologyTree("dummy.db");
+		ont.Initialize();
+		ont.AddNode("Teaching", "Professor");
+		ont.AddNode("Research", "Professor");
+		ont.AddNode("Proposals", "Research");
+		//System.out.println(dbHandler.PathToNode("Research", "")); // path from root to Research
+		//System.out.println(dbHandler.NodeChildren("Professor")); // children of Professor
+		System.out.println(ont.PathThrough("Research"));
+		ont.RemoveNode("Proposals");
+		System.out.println("removed Proposals");
+		System.out.println(ont.PathThrough("Research"));
+		ont.Close();
+	}
+}
