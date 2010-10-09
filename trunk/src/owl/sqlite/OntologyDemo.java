4cef0efffbc9a9408af6b5ab2b0aaf3038b92866
w.w.mensah@gmail.com <w.w.mensah@gmail.com@f61b3770-9b39-c3b9-6f90-abcdacec213d>
2010-10-09 15:34:46 +0000

    added method to return top x paths that match keywords

--- trunk/src/owl/sqlite/OntologyDemo.java
+++ trunk/src/owl/sqlite/OntologyDemo.java
@@ -4,17 +4,31 @@ package owl.sqlite;
 public class OntologyDemo {
 	
 	public static void main(String[] args) throws Exception{
-		OntologyTree ont = new OntologyTree("res/ontology.db");
+		OntologyHandler ont = new OntologyHandler("res/ontology.db");
 		ont.Initialize();
 		ont.AddNode("Teaching", "Professor");
 		ont.AddNode("Research", "Professor");
 		ont.AddNode("Proposals", "Research");
+		
 		//System.out.println(dbHandler.PathToNode("Research", "")); // path from root to Research
 		//System.out.println(dbHandler.NodeChildren("Professor")); // children of Professor
+		
 		System.out.println(ont.PathThrough("Research"));
-		ont.RemoveNode("Proposals");
-		System.out.println("removed Proposals");
-		System.out.println(ont.PathThrough("Research"));
+		
+		//ont.RemoveNode("Proposals");
+		//System.out.println(ont.PathThrough("Research"));
+		
+		System.out.println("\nPaths:");
+		String[] keywords = {"Proposals", "Teaching", "Research"};
+		String[][] paths = ont.SimilarityMatch(keywords);
+		
+		for (int i = 0; i < paths[0].length; i++){
+			System.out.println(paths[i][0] + ": " + paths[i][1]);
+		}
+		
+		System.out.println("\nBest Path: ");
+		String[] best = ont.BestMatch(paths, 1);
+		ont.PrintArray(best);
 		ont.Close();
 	}
 }
---
+++ trunk/src/owl/sqlite/OntologyHandler.java
@@ -0,0 +1,113 @@
+package owl.sqlite;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class OntologyHandler extends OntologyTree {
+
+	public OntologyHandler(String filename) throws Exception {
+		super(filename);
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void Initialize(){
+		try{
+			stat.executeUpdate("create table Ontology (ID INTEGER PRIMARY KEY AUTOINCREMENT, Title VARCHAR(500) UNIQUE)");
+			stat.executeUpdate("create table Relationships (Parent VARCHAR(500), Child VARCHAR(500), PRIMARY KEY (Parent, Child))");
+			stat.execute("INSERT INTO Ontology(title) VALUES ('Professor')");
+			System.out.println("Created tables: Ontology, Relationships. Database is ready!");
+		}catch(Exception e){
+			System.out.println("Database & tables already exist");
+		}		
+	}
+	
+	public void ConvertToOwl(){
+		/*
+		 * Converts the contents of the database to an OWL file
+		 */
+	}
+	
+	private int RankPath(String[] keywords, String path){
+		/*
+		 * Gives a count of how many words in keywords were matched in path		 * 
+		 * @param keywords - words to be matched
+		 * @param path - string to be searched for keywords
+		 * @return count - number of matches found
+		 * hello world
+		 */
+		int count = 0;
+		for (int i = 0; i < keywords.length; i++){
+			if (path.lastIndexOf((keywords[i])) > 0){
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public String[][] SimilarityMatch(String[] keywords){
+		/*
+		 * Returns the best path-match in the tree containing keywords
+		 * @param keywords - array of keywords to be matched
+		 * @return best_path - string representing path in tree with most matches to keywords
+		 */
+		
+		String path = "";
+		String[][] top = new String[keywords.length][keywords.length]; // rank, path
+		int cnt = 0;
+		for (int i = 0; i < keywords.length; i++){
+			
+			// for each keyword, find a path in the tree through it
+			path = PathToNode(keywords[i], "");
+			
+			top[cnt][0] = Integer.toString(RankPath(keywords, path));
+			top[cnt][1] = path;
+			cnt++;
+		}
+		return top;
+	}
+	
+	public String[] BestMatch(String[][] paths, int max){
+		/*
+		 * Returns the top 'max' matching paths
+		 * @param paths - ranked paths. path[x][0] = rank, path[x][1] = path
+		 * @param max - number of top paths to return
+		 * @return - 1d array of top 'max' paths
+		 */
+		if (max > paths.length){
+			// max can't be greater than the number of paths
+			max = paths.length;
+		}
+		String[] best = new String[max];		
+		Arrays.sort(paths, new RankComparator());
+		
+		for (int i = 0; i < max; i++){
+			best[i] = paths[i][1];
+		}
+		return best;
+	}
+	
+	class RankComparator implements Comparator<Object>
+	{
+	  public int compare(Object obj1, Object obj2)
+	  {
+		  String[] str1 = (String[])obj1;
+		  String[] str2 = (String[])obj2;
+		  
+	    if (Integer.parseInt(str1[0]) > Integer.parseInt(str2[0]))
+	    	return -1;
+	    else if (Integer.parseInt(str1[0]) < Integer.parseInt(str2[0]))
+	    	return 1;
+	    else
+	    	return 0;
+	  }
+	}
+	
+	public void PrintArray(String[] array){
+		for (int i = 0; i < array.length; i++){
+			System.out.println(array[i]);
+		}
+	}
+	
+	
+	
+}
